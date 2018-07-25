a72115fcd56ebda982841440a523bd0bb81c1f5c
陈为 <chenwei2000121@163.com>
2018-07-25 19:39:54 +0800

    Initial commit

---
+++ SwiftDictQuery/SwiftDictQuery.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDictQuery.h
+//  SwiftDictQuery
+//
+//  Created by 陈为 on 2018/7/25.
+//  Copyright © 2018年 David Chen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftDictQuery.
+FOUNDATION_EXPORT double SwiftDictQueryVersionNumber;
+
+//! Project version string for SwiftDictQuery.
+FOUNDATION_EXPORT const unsigned char SwiftDictQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDictQuery/PublicHeader.h>
+
+
